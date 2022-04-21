3f0a8d69363a04cb9c9223a82302bb58dff3eeb9
Marie-Christine <m-ccloutier@hotmail.com>
2022-04-21 18:23:31 -0400

    add et delete php et css

--- site/add.php
+++ site/add.php
@@ -1,16 +1,51 @@
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
+<style>
+    form {
+    
+    }
+</style>
 <head>
     <title>add</title>
-    
-
-
+    <link rel="stylesheet" a href="site/css/add.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 
-<h1>My First Heading</h1>
-<?php echo "hi"; ?>
-<p>My first paragraph.</p>
+<div class="container">
+  <form>
+      <h1>Ajout d'une microbrasserie</h1>
+    <div class="row">
+      <h4>Information sur la microbrasserie</h4>
+      <div class="input-group input-group-icon">
+        <input type="text" placeholder="Nom de la microbrasserie"/>
+        <div class="input-icon"><i class="fa fa-user"></i></div>
+      </div>
+      <div class="input-group input-group-icon">
+        <input type="text" placeholder="Adresse"/>
+        <div class="input-icon"><i class="fa fa-address-card"></i></div>
 
+        <div class="input-group input-group-icon">
+        <input type="text" placeholder="Ville"/>
+        <div class="input-icon"><i class="fa fa-address-card"></i></div>
+
+        <div class="input-group input-group-icon">
+        <input type="text" placeholder="Code postal"/>
+        <div class="input-icon"><i class="fa fa-address-card"></i></div>
+      </div>
+      </div>
+      </div>
+      <div class="input-group input-group-icon">
+        <input type="text" placeholder="#Permis"/>
+        <div class="input-icon"><i class="fa fa-ticket"></i></div>
+      </div>
+      <div class="input-group input-group-icon">
+        <input type="email" placeholder="Email Adress"/>
+        <div class="input-icon"><i class="fa fa-envelope"></i></div>
+      </div>
+
+      <button type="button">Ajouter à la liste des microbrasseries</button>
+    
+    </div>
 </body>
 </html> 
